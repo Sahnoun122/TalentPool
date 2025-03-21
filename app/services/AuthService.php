8221ad25326f4784a4c730035805d8ce3fa74fa7
sahnoun122 <khadijasahnoun70@gmail.com>
2025-03-21 15:09:18 +0000

    ajouter le services de la partie authentification

---
+++ app/services/AuthService.php
@@ -0,0 +1,40 @@
+<?php 
+
+// app/Services/AuthService.php
+namespace App\Services;
+
+use App\Repositories\UserRepositoryInterface;
+use Illuminate\Support\Facades\Hash;
+
+class AuthService
+{
+    protected $userRepository;
+
+    public function __construct(UserRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function register(array $data)
+    {
+        $data['password'] = bcrypt($data['password']);
+        return $this->userRepository->register($data);
+    }
+
+    public function login(array $credentials)
+    {
+        $user = $this->userRepository->Connecter($credentials['email']);
+
+        if (!$user || !Hash::check($credentials['password'], $user->password)) {
+            return null;
+        }
+
+        return $user;
+    }
+
+    public function modifierMotDePasse($user, string $newPassword)
+    {
+        $this->userRepository->modifierMotDePasse($user, $newPassword);
+    }
+}
+
