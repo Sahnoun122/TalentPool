2f3193e2414e95848b55f60ca4d24eb4268df591
sahnoun122 <khadijasahnoun70@gmail.com>
2025-03-22 14:52:00 +0000

    ajouter le token dans event service provider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -1,9 +1,31 @@
 <?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use App\Events\CandidatureStatutModifie;
 use App\Listeners\EnvoyerNotificationStatut;
 
-protected $listen = [
-    CandidatureStatutModifie::class => [
-        EnvoyerNotificationStatut::class,
-    ],
-];
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * Les écouteurs d'événements.
+     *
+     * @var array
+     */
+    protected $listen = [
+        CandidatureStatutModifie::class => [
+            EnvoyerNotificationStatut::class,
+        ],
+    ];
+
+    /**
+     * Enregistrer les événements et les écouteurs.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+    }
+}
