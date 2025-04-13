0e3e2114be2e351a41caf74db8590bfb08d7e8b9
sahnoun122 <khadijasahnoun70@gmail.com>
2025-04-13 22:38:53 +0100

    la creations des routess

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\CandidatureController;
+use App\Http\Controllers\API\StatistiqueController;
 
 // Route::get('/', function () {
 //     return response()->json(['message' => 'Welcome to the API']);
@@ -9,3 +12,30 @@ use Illuminate\Support\Facades\Route;
 Route::get('/register', [App\Http\Controllers\authViews::class, 'register'])->name('register');
 Route::get('/connecter', [App\Http\Controllers\authViews::class, 'connecter'])->name('connecter');
 Route::get('/resetpassword', [App\Http\Controllers\authViews::class, 'resetpassword'])->name('resetpassword');
+
+
+
+
+
+Route::prefix('candidatures')->group(function () {
+    Route::post('/', [CandidatureController::class, 'store'])
+        ->name('candidatures.store');
+    
+    Route::get('/', [CandidatureController::class, 'index'])
+        ->name('candidatures.index');
+    
+    Route::delete('/{id}', [CandidatureController::class, 'destroy'])
+        ->name('candidatures.destroy');
+    
+    Route::put('/{id}/statut', [CandidatureController::class, 'modifierStatut'])
+        ->name('candidatures.modifierStatut');
+});
+
+Route::prefix('statistiques')->group(function () {
+    Route::get('/admin', [StatistiqueController::class, 'statistiquesAdmin'])
+        ->name('statistiques.admin');
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    
+});
